a15c61e9301368cdb6386ef371ee6237c1eedd3d
1E10 Laptop <sample@bths.edu>
2022-09-21 12:14:31 -0400

    v 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+        String chicken = "chicken";
+        System.out.println(chicken);
+        String message = "is yummy";
+        System.out.println(message);
+    }
 }
